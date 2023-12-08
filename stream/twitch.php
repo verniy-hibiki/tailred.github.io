e4e9c8780fbf9da64b7a69b69c0571402a71d6f8
Verniy <11333703+verniy-hibiki@users.noreply.github.com>
2023-12-08 17:02:15 -0500

    Update twitch.php

--- stream/twitch.php
+++ stream/twitch.php
@@ -13,13 +13,13 @@
 	.i span{ color:#EEE; }
 </style>
 <body>
-	<object type="application/x-shockwave-flash" width="798" height="483" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=albrecht84">
-		<param name="flashvars" value="channel=albrecht84&auto_play=true">
+	<object type="application/x-shockwave-flash" width="798" height="483" data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=verniy">
+		<param name="flashvars" value="channel=verniy&auto_play=true">
 		<param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf">
 		<param name="allowFullScreen" value="true">
 		<param name="allowscriptaccess" value="always">
 	</object>
-	<iframe src="http://www.twitch.tv/chat/embed?channel=albrecht84&popout_chat=true" width="310" height="477" scrolling="no"></iframe>
+	<iframe src="http://www.twitch.tv/chat/embed?channel=verniy&popout_chat=true" width="310" height="477" scrolling="no"></iframe>
 	<center style="width:792px">
 		<span class="i">
 			Viewers:
@@ -36,7 +36,7 @@
 		function info(){
 			var s=document.createElement("script");
 			var h=document.head;
-			s.src="http://api.justin.tv/api/stream/list.json?jsonp=jsonp&channel=albrecht84";
+			s.src="http://api.justin.tv/api/stream/list.json?jsonp=jsonp&channel=verniy";
 			h.insertBefore(s, h.firstChild);
 			h.removeChild(s);
 			setTimeout("info()",30000);
